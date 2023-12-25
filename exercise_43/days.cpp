adae3e2015f270395fcbe70af654f1795387b3a4
Karim Benkhira <karimbenkhira.contact@gmail.com>
2023-12-25 04:32:33 +0100

    Day Converter

--- exercise_43/days.cpp
+++ exercise_43/days.cpp
@@ -3,66 +3,74 @@
 
 using namespace std;
 
-enum enDays{Sunday = 1,Monday = 2,Tuesday = 3,
-            Wednesday = 4,Thursday = 5,Friday = 6,Saturday = 7};
+enum enDays{Sunday = 1, Monday = 2, Tuesday = 3,
+            Wednesday = 4, Thursday = 5, Friday = 6, Saturday = 7};
+
 int ReadNumber(string Message)
 {
   int Number;
-  do {
+  do
+  {
     cout << Message << endl;
     cin >> Number;
-  } while(Number < 1 || Number > 7);
+  } while (Number < 1 || Number > 7);
   return Number;
 }
 
 enDays CheakDays(int Number)
 {
-  if(Number == 1)
-    return (enDays::Sunday);
-  else if(Number == 2)
+  switch (Number)
+  {
+  case 1:
+    return enDays::Sunday;
+  case 2:
     return enDays::Monday;
-  else if (Number == 3)
+  case 3:
     return enDays::Tuesday;
-  else if(Number == 4)
+  case 4:
     return enDays::Wednesday;
-  else if(Number == 5)
+  case 5:
     return enDays::Thursday;
-  else if(Number == 6)
+  case 6:
     return enDays::Friday;
-  else
+  case 7:
     return enDays::Saturday;
+  default:
+    return enDays::Sunday;
+  }
 }
 
 void PrintDay(enDays Day)
 {
-  switch (Day) {
-    case enDays::Sunday:
-      cout << "Sunday" << endl;
-      break;
-    case enDays::Monday:
-      cout << "Monday" << endl;
-      break;
-    case enDays::Tuesday:
-      cout << "Tuesday" << endl;
-      break;
-    case enDays::Wednesday:
-      cout << "Wednesday" << endl;
-      break;
-    case enDays::Thursday:
-      cout << "Thursday" << endl;
-      break;
-    case enDays::Friday:
-      cout << "Friday" << endl;
-      break;
-    case enDays::Saturday:
-      cout << "Saturday" << endl;
-      break;
+  switch (Day)
+  {
+  case enDays::Sunday:
+    cout << "Sunday" << endl;
+    break;
+  case enDays::Monday:
+    cout << "Monday" << endl;
+    break;
+  case enDays::Tuesday:
+    cout << "Tuesday" << endl;
+    break;
+  case enDays::Wednesday:
+    cout << "Wednesday" << endl;
+    break;
+  case enDays::Thursday:
+    cout << "Thursday" << endl;
+    break;
+  case enDays::Friday:
+    cout << "Friday" << endl;
+    break;
+  case enDays::Saturday:
+    cout << "Saturday" << endl;
+    break;
   }
 }
 
 int main()
 {
-  PrintDay(CheakDays(ReadNumber("Please Enter a Number between 1 and 7 : ")));
+  PrintDay(CheakDays(ReadNumber("Please Enter a Number between 1 and 7:")));
 
   return 0;
 }
