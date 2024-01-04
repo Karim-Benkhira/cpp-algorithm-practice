c6167d077558e2e02d615f34a0df25e05d72bde2
Karim Benkhira <karimbenkhira.contact@gmail.com>
2024-01-04 02:40:35 +0100

    # Random Number Array Generator

---
+++ exercise_70/random.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int RanDom(int From,int To)
+{
+    int RandNB = rand() % (To - From + 1) + 1;
+    return RandNB;
+}
+
+void ReadRandomNumberarray(int array[],int &arraylength)
+{
+    cout << "Please Enter Number of element : "<< endl;
+    cin >> arraylength;
+
+    for(int i = 0;i < arraylength;i++)
+    {
+        array[i] = RanDom(1,100);
+    }
+}
+
+void Printarray(int array[100],int arralength)
+{
+    for(int i = 0;i < arralength;i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arrLength;
+    int array[100];
+
+    ReadRandomNumberarray(array,arrLength);
+    cout << "Array Element : ";
+    Printarray(array,arrLength);
+
+    return 0;
+    
+}
